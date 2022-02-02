6fb937f7dc959611df77b9b9a5eb5bdd1744b17b
Amrut Sagar <amrutsagar06@gmail.com>
2022-02-02 18:52:26 +0530

    added components

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import "./index.css";
+import Navigation from "./componets/Navigation";
+import Home from './componets/Home';
+import Products from './componets/Products';
 
 
 function App() {
   return (
     <div className="App">
+     <Navigation />
+     <Home/>
+     <Products/>
+     <Signin />
      
     </div>
   );
---
+++ weatherapp/src/componets/Navigation/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './styles.css';
+const Navigation = () => {
+   return (
+<div className="nav-container">
+    <ul className="nav-list">
+    <li>Home</li>
+    <li>Product</li>
+    <li>Signin</li>
+    </ul>
+    
+</div>
+   )
+}
+   
+export default Navigation;

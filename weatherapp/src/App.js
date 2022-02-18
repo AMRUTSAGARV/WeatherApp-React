0cdd399464267cda91a1349851d3727b91fbb0a6
Amrut Sagar <amrutsagar06@gmail.com>
2022-02-19 02:01:07 +0530

    env file created

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -2,10 +2,12 @@
 import React, { useState } from 'react';
 import "./index.css";
 
-const api = {
-  key: "e26f422fd39b9eaae9e3634e7dfb4a93",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+require('dotenv').config();
+
+// const api = {
+//   key: "e26f422fd39b9eaae9e3634e7dfb4a93",
+//   base: "https://api.openweathermap.org/data/2.5/"
+// }
 
 function App() {
 

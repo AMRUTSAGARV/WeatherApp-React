d4a590fcd7c0078d5997866e91a00b697891a1eb
Amrut Sagar <amrutsagar06@gmail.com>
2022-02-19 23:35:02 +0530

    api key generated

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -2,11 +2,14 @@
 import React, { useState } from 'react';
 import "./index.css";
 
-require('dotenv').config();
+// require('dotenv').config();
 
 
 
-//
+const api = {
+  key: "7d4ef5d23648c7c62acd3ccfa1a0a30b",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 
 function App() {

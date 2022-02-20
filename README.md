39e93a6d97abc73450f807e97c48c42bbd9c2fad
Amrut Sagar <amrutsagar06@gmail.com>
2022-02-20 19:20:45 +0530

    weather app react

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WeatherApp-React
+
+
+<!-- Learn how to make a Weather app in React JS, we use the Open Weather Map API to make calls to a restful api to return the current weather data. This uses the Modern Javascript Fetch AP along with React hooks and conditonals. -->
--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,24 +1,9 @@
-
 import React, { useState } from 'react';
 import "./index.css";
 
-// require('dotenv').config();
-
-
-
 const api = {
-  
   base: "https://api.openweathermap.org/data/2.5/"
 }
-
-//my api (93)
-// const api = {
-//   key: "e26f422fd39b9eaae9e3634e7dfb4a93",
-//   base: "https://api.openweathermap.org/data/2.5/"
-// }
-
-
-
 
 
 function App() {
